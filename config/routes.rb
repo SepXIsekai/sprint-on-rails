ab3ef76429525f05dbe3531ca20c11e9f7567922
Sepx <sepx@Sepxs.local>
2025-08-09 14:30:07 +0700

    separate role user and admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  resources :articles
+  resources :articles, only: [ :index, :show ]
+
+  namespace :admin do
+    resources :articles
+    root "articles#index"
+  end
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root "articles#index"
