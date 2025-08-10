6c77080a2e116a241fd58706f35750b2700ce368
Sepx <sepx@Sepxs.local>
2025-08-10 16:34:18 +0700

    remove render

--- Gemfile
+++ Gemfile
@@ -46,7 +46,7 @@ group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
   gem "rspec-rails"
-
+  gem "launchy"
   # Static analysis for security vulnerabilities [https://brakemanscanner.org/]
   gem "brakeman", require: false
 
--- spec/system/user_can_see_article_spec.rb
+++ spec/system/user_can_see_article_spec.rb
@@ -1,8 +1,19 @@
 require 'rails_helper'
 
 describe 'User can see article' do
+  context 'an article exists' do
+    before do
+      Article.create(
+        description: "a brief description of the article",
+        content_attributes: {
+          title: 'Sprint on Rails'
+        }
+      )
+    end
   it "display article on page" do
     visit root_path
     expect(page).to have_content('Sprint on Rails')
+    save_and_open_page
+  end
   end
 end
